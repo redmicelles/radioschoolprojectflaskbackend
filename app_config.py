2deeac3c8fe17892ad61285a09f216db8d4824c3
Timmy-Edibo <timothyedibo@gmail.com>
2022-12-10 22:07:04 -0800

    Initial commit

--- app_config.py
+++ app_config.py
@@ -3,13 +3,14 @@ import connexion
 from flask_sqlalchemy import SQLAlchemy
 import pathlib
 from flask_migrate import Migrate
+from config import Config
 
 
 basedir = pathlib.Path(__file__).parent.resolve()
 
 connex_app = connexion.App(__name__, specification_dir=basedir)
 app = connex_app.app
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:Password123&!@172.23.0.1:3308/radioschool"
+app.config["SQLALCHEMY_DATABASE_URI"] = Config.SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
